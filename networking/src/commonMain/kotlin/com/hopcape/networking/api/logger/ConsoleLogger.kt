9e84720d8453a2f38891653b23be010a3bd75f55
aumaidKh <aumaidm.m.c@gmail.com>
2025-01-26 13:37:18 +0530

    Changes: - Added Kdoc for Logger, consoleLogger and DependencyContainer, DependencyFactory - Added Test cases for DependencyContainer.kt, NetworkingDependencyContainer

--- networking/src/commonMain/kotlin/com/hopcape/networking/api/logger/ConsoleLogger.kt
+++ networking/src/commonMain/kotlin/com/hopcape/networking/api/logger/ConsoleLogger.kt
@@ -1,5 +1,23 @@
 package com.hopcape.networking.api.logger
 
+/**
+ * A logger implementation that outputs log messages to the console.
+ *
+ * This logger prepends the message with the prefix "HopcapeNetworking:" and prints it
+ * to the standard output using `println()`. It is intended for use in scenarios
+ * where simple console logging is needed for debugging or development purposes.
+ *
+ * ## Example Usage:
+ * ```kotlin
+ * consoleLogger.log("This is a log message.")
+ * // Output: HopcapeNetworking: This is a log message.
+ * ```
+ *
+ * This logger is internal to the library, and it is intended for use within the
+ * `com.hopcape.networking` package.
+ *
+ * @see Logger
+ */
 internal val consoleLogger = Logger {
     println("HopcapeNetworking: $it")
 }
--- networking/src/commonMain/kotlin/com/hopcape/networking/api/logger/Logger.kt
+++ networking/src/commonMain/kotlin/com/hopcape/networking/api/logger/Logger.kt
@@ -1,5 +1,25 @@
 package com.hopcape.networking.api.logger
 
+/**
+ * A functional interface representing a logger that outputs log messages.
+ *
+ * This interface defines a single method `log()` that accepts a string message
+ * and outputs it in a way that can be implemented for various logging mechanisms
+ * (e.g., console logging, file logging, etc.).
+ *
+ * ## Example Usage:
+ * ```kotlin
+ * val logger = Logger { message -> println("Log: $message") }
+ * logger.log("This is a log message.")
+ * ```
+ *
+ * @author Murtaza Khursheed
+ */
 fun interface Logger {
+    /**
+     * Logs the provided message.
+     *
+     * @param message The message to be logged.
+     */
     fun log(message: String)
 }
