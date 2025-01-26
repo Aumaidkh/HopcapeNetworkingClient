3a00a3095ebcfecb38c891dcf5e2c99d264a182a
aumaidKh <aumaidm.m.c@gmail.com>
2025-01-26 13:21:08 +0530

    Changes: - Added Kdoc for RequestBuilder - Added Test cases for RequestBuilderTest

--- networking/src/commonMain/kotlin/com/hopcape/networking/api/request/RequestBuilder.kt
+++ networking/src/commonMain/kotlin/com/hopcape/networking/api/request/RequestBuilder.kt
@@ -3,26 +3,84 @@ package com.hopcape.networking.api.request
 import com.hopcape.networking.api.Url
 import com.hopcape.networking.api.request.methods.HttpMethod
 
+/**
+ * A builder class to help construct a `NetworkRequest` object with optional configurations.
+ *
+ * This class provides a convenient way to set different properties of a `NetworkRequest` such as the URL,
+ * HTTP method, headers, and body. It uses a fluent interface to allow chaining of methods, making it easy
+ * to build a customized `NetworkRequest`.
+ *
+ * ## How it Works:
+ * - Use `setUrl()` to specify the URL for the request.
+ * - Use `setMethod()` to set the HTTP method (e.g., `GET`, `POST`).
+ * - Use `setHeaders()` to set any headers for the request.
+ * - Use `setBody()` to specify the request body if needed.
+ * - Finally, use `build()` to retrieve the constructed `NetworkRequest`.
+ *
+ * ## Example Usage:
+ * ```kotlin
+ * val request = com.hopcape.networking.api.request.RequestBuilder()
+ *     .setUrl(Url("https://api.example.com"))
+ *     .setMethod(HttpMethod.GET)
+ *     .setHeaders(mapOf("Authorization" to "Bearer token"))
+ *     .setBody("request body")
+ *     .build()
+ * ```
+ *
+ * @constructor Creates a new `RequestBuilder` instance.
+ * @author Murtaza Khursheed
+ */
 class RequestBuilder {
     private var request: NetworkRequest = NetworkRequest()
 
-    fun setUrl(url: Url): NetworkRequest {
+    /**
+     * Sets the URL for the network request.
+     *
+     * @param url The URL to be used in the request.
+     * @return The updated `RequestBuilder` instance.
+     */
+    fun setUrl(url: Url): RequestBuilder {
         request = request.copy(url = url.value)
-        return request
+        return this
+    }
 
-    }
-    fun setMethod(method: HttpMethod): NetworkRequest {
+    /**
+     * Sets the HTTP method for the network request.
+     *
+     * @param method The HTTP method (e.g., `GET`, `POST`) to be used in the request.
+     * @return The updated `RequestBuilder` instance.
+     */
+    fun setMethod(method: HttpMethod): RequestBuilder {
         request = request.copy(method = method)
-        return request
+        return this
     }
-    fun setHeaders(headers: Map<String, String>): NetworkRequest {
+
+    /**
+     * Sets the headers for the network request.
+     *
+     * @param headers The headers to be included in the request.
+     * @return The updated `RequestBuilder` instance.
+     */
+    fun setHeaders(headers: Map<String, String>): RequestBuilder {
         request = request.copy(requestHeaders = headers)
-        return request
+        return this
     }
-    fun setBody(body: String): NetworkRequest {
+
+    /**
+     * Sets the body for the network request.
+     *
+     * @param body The body to be included in the request.
+     * @return The updated `RequestBuilder` instance.
+     */
+    fun setBody(body: String): RequestBuilder {
         request = request.copy(body = body)
-        return request
+        return this
     }
-    fun build() =
-        request
+
+    /**
+     * Builds and returns the constructed `NetworkRequest` instance.
+     *
+     * @return The final `NetworkRequest` instance with all the configurations set.
+     */
+    fun build() = request
 }
---
+++ networking/src/commonTest/kotlin/com/hopcape/networking/api/request/RequestBuilderTest.kt
@@ -0,0 +1,56 @@
+package com.hopcape.networking.api.request
+
+import com.hopcape.networking.api.Url
+import com.hopcape.networking.api.request.methods.HttpMethod
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class RequestBuilderTest {
+
+    @Test
+    fun `test setUrl should set URL correctly`() {
+        val url = Url("https://api.example.com")
+        val request = RequestBuilder().setUrl(url).build()
+        assertEquals("https://api.example.com", request.url)
+    }
+
+    @Test
+    fun `test setMethod should set HTTP method correctly`() {
+        val request = RequestBuilder().setMethod(HttpMethod.POST).build()
+        assertEquals(HttpMethod.POST, request.method)
+    }
+
+    @Test
+    fun `test setHeaders should set headers correctly`() {
+        val headers = mapOf("Authorization" to "Bearer token")
+        val request = RequestBuilder().setHeaders(headers).build()
+        assertEquals(headers, request.requestHeaders)
+    }
+
+    @Test
+    fun `test setBody should set body correctly`() {
+        val body = "request body"
+        val request = RequestBuilder().setBody(body).build()
+        assertEquals(body, request.body)
+    }
+
+    @Test
+    fun `test build should create a valid NetworkRequest`() {
+        val url = Url("https://api.example.com")
+        val headers = mapOf("Authorization" to "Bearer token")
+        val body = "request body"
+
+        val request = RequestBuilder()
+            .setUrl(url)
+            .setMethod(HttpMethod.GET)
+            .setHeaders(headers)
+            .setBody(body)
+            .build()
+
+        assertEquals("https://api.example.com", request.url)
+        assertEquals(HttpMethod.GET, request.method)
+        assertEquals(headers, request.requestHeaders)
+        assertEquals(body, request.body)
+    }
+}
